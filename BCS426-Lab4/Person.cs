2161d4c11fa17c1399651f961c88b3f3e7c134ff
alexander-harmaty <92049896+alexander-harmaty@users.noreply.github.com>
2022-03-13 18:30:40 -0400

    Add project files.

---
+++ BCS426-Lab4/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Wrox.ProCSharp.Generics
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string f = "none", string l = "none")
+        {
+            FirstName = f;
+            LastName = l;
+        }
+
+        public override string ToString() => $"{FirstName} {LastName}";
+    }
+
+}
